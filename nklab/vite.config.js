345b7d74682486c1bba7bd695be4e9a28ab28034
FranccoL <leofranco96@gmail.com>
2025-02-14 19:20:57 -0300

    Vercel

--- nklab/vite.config.js
+++ nklab/vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Adiciona a base correta para produção
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    historyApiFallback: true, // Garante suporte ao fallback
+  },
 })
